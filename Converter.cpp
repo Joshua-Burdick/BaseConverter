c097d8aae928b4444bac94d142154c70c83aeab3
Joshua-Burdick <joshburdick1@hotmail.com>
2021-09-15 11:37:31 -0500

    Bug fix

--- Converter.cpp
+++ Converter.cpp
@@ -12,7 +12,7 @@ int Converter::toDecimal(vector<int> num, int base) {
     return sum;
 }
 
-// Convert decimal to binary /* NON-WORKING */
+// Convert decimal to binary
 vector<int> Converter::toBase(int decimal, int base) {
 
     sum = decimal;
--- Inclusion.h
+++ Inclusion.h
@@ -1,6 +1,10 @@
+#pragma once
+
 #include <iostream>
 #include <string>
 #include <vector>
 #include <cmath>
 
 using namespace std;
+
+string characters = "0123456789ABCDEF";
